95275577b0b3192616671bc8aea33139d0522569
William Eddie Plange <Wilplanh@gmail.com>
2025-08-17 20:55:41 +0000

    Database

--- MySQLServer.py
+++ MySQLServer.py
@@ -19,7 +19,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e: # <-- required explicit exception
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
